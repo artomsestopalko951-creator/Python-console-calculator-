b25cbfb84fa49cf6331201add5571c4bc20caf45
Okuroxx <225953643+artomsestopalko951-creator@users.noreply.github.com>
2025-08-12 15:08:40 +0300

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,18 @@
+n1 = int(input("enter 1nd number "))
+wtd = input("what to do? +, -, * or ×, / ? ")
+n2 = int(input("enter 2nd number "))
+
+if wtd == "+":
+    print(n1 + n2)
+elif wtd == "-":
+    print(n1 - n2)
+elif wtd == "*" or wtd == "×":
+    print(n1 * n2)
+elif wtd == "/":
+    if n2 == 0:
+        print("Error: division by zero!")
+    else:
+        print(n1 / n2)
+else:
+    print("bro what a u doing?")
+except ValueError: print("enter a number! not text!")
